6dfd3075b1a655156242a085eecd080b43528c52
liming <827182486@qq.com>
2020-07-07 20:19:33 +0800

    update 第五章

--- 第五章/pexpect/simple1.py
+++ 第五章/pexpect/simple1.py
@@ -1,21 +1,22 @@
-import pxssh
+from pexpect import pxssh
 import getpass
+
 try:
-    s = pxssh.pxssh()
-    hostname = raw_input('hostname: ')
-    username = raw_input('username: ')
-    password = getpass.getpass('password: ')
-    s.login (hostname, username, password)
-    s.sendline ('uptime')  # run a command
-    s.prompt()             # match the prompt
-    print s.before         # print everything before the prompt.
-    s.sendline ('ls -l')
-    s.prompt()
-    print s.before
-    s.sendline ('df')
-    s.prompt()
-    print s.before
-    s.logout()
-except pxssh.ExceptionPxssh, e:
-    print "pxssh failed on login."
-    print str(e)
+    s = pxssh.pxssh() #创建pxssh对象
+    hostname = input('hostname: ')
+    username = input('username: ')
+    password = getpass.getpass('password: ')  #接收密码输入
+    s.login(hostname, username, password) #建立ssh连接
+    s.sendline('uptime')  # run a command #运行uptime命令
+    s.prompt()  # match the prompt #匹配系统提示符
+    print(s.before)  # print everything before the prompt.  #打印出现系统提示符前的命令输出
+    s.sendline('ls -l') #运行命令
+    s.prompt()#匹配系统提示符
+    print(s.before) #打印出现系统提示符前的命令输出
+    s.sendline('df')#运行命令
+    s.prompt()#匹配系统提示符
+    print(s.before)#打印出现系统提示符前的命令输出
+    s.logout() #断开ssh连接
+except pxssh.ExceptionPxssh as e:
+    print("pxssh failed on login.")
+    print(str(e))
--- 第五章/pexpect/simple2.py
+++ 第五章/pexpect/simple2.py
@@ -1,15 +1,15 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals	#使用unicode编码
 
 import pexpect
 import sys
 
 child = pexpect.spawnu('ftp ftp.openbsd.org')
-child.expect('(?i)name .*: ')
+child.expect('(?i)name .*: ') #(?i)忽略大小写
 child.sendline('anonymous')
 child.expect('(?i)password')
 child.sendline('pexpect@sourceforge.net')
 child.expect('ftp> ')
-child.sendline('bin')
+child.sendline('bin') #开启二进制传输
 child.expect('ftp> ')
 child.sendline('get robots.txt')
 child.expect('ftp> ')
